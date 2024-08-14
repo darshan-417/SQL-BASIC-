91ec7622b9647a1a2d20b6233cb6983346fdf3a0
Darshan R <143541626+darshan-417@users.noreply.github.com>
2024-08-14 23:08:04 +0530

    Add files via upload

--- darshan_student.sql
+++ darshan_student.sql
@@ -16,30 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `employee`
+-- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employee` (
-  `emp_id` int NOT NULL,
-  `ename` varchar(30) DEFAULT NULL,
-  `job_desc` varchar(20) DEFAULT NULL,
-  `salary` int DEFAULT NULL,
-  `Hire_Date` date DEFAULT NULL,
-  PRIMARY KEY (`emp_id`)
+CREATE TABLE `student` (
+  `id` int NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `gpa` decimal(3,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employee`
+-- Dumping data for table `student`
 --
 
-LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Ram','ADMIN',1000000,NULL),(2,'Harini','MANAGER',2500000,NULL),(3,'George','SALES',2000000,NULL),(4,'Ramya','SALES',1300000,NULL),(5,'Meena','HR',2000000,NULL),(6,'Ashok','MANAGER',3000000,NULL),(7,'Abdul','HR',2000000,NULL),(8,'Ramya','ENGINEER',1000000,NULL),(9,'Raghu','CEO',8000000,NULL),(10,'Arvind','MANAGER',2800000,NULL),(11,'Akshay','ENGINEER',1000000,NULL),(12,'John','ADMIN',2200000,NULL),(13,'Abinaya','ENGINEER',2100000,NULL);
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'Aarthi',7.60),(2,'Anitha',8.50),(3,'Anitha',8.50),(4,'Arul',8.20),(5,'Ashwin',7.60);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
